b02f07f0777851983a351a9888557cfbfeb60bf2
Otari Pkhovelishvili <opkhovelishvili@gmail.com>
2024-05-23 22:08:19 +0400

    s

--- app/api/send-email/route.ts
+++ app/api/send-email/route.ts
@@ -1,40 +1,40 @@
-import nodemailer from "nodemailer";
+// import nodemailer from "nodemailer";
 
-// Configure the email transporter
-const transporter = nodemailer.createTransport({
-  service: "gmail",
-  auth: {
-    user: process.env.MY_EMAIL,
-    pass: process.env.MY_PASSWORD,
-  },
-});
+// // Configure the email transporter
+// const transporter = nodemailer.createTransport({
+//   service: "gmail",
+//   auth: {
+//     user: process.env.MY_EMAIL,
+//     pass: process.env.MY_PASSWORD,
+//   },
+// });
 
-export default async function handler(req: Request) {
-  if (req.method === "POST") {
-    const { firstname, lastname, email, text, number } = req.body;
+// export default async function handler(req: Request) {
+//   if (req.method === "POST") {
+//     const { firstname, lastname, email, text, number } = req.body;
 
-    const mailOptions = {
-      from: "your-email@gmail.com",
-      to: "recipient-email@example.com",
-      subject: "New Form Submission",
-      text: `
-        First Name: ${firstname}
-        Last Name: ${lastname}
-        Email: ${email}
-        Text: ${text}
-        Number: ${number}
-      `,
-    };
+//     const mailOptions = {
+//       from: "your-email@gmail.com",
+//       to: "recipient-email@example.com",
+//       subject: "New Form Submission",
+//       text: `
+//         First Name: ${firstname}
+//         Last Name: ${lastname}
+//         Email: ${email}
+//         Text: ${text}
+//         Number: ${number}
+//       `,
+//     };
 
-    try {
-      await transporter.sendMail(mailOptions);
+//     try {
+//       await transporter.sendMail(mailOptions);
 
-      return new Response({ message: "Form submitted successfully" });
-    } catch (error) {
-      console.error("Error sending email:", error);
-      new Response({ message: "Failed to submit the form" });
-    }
-  } else {
-    new Response({ message: "Method Not Allowed" });
-  }
-}
+//       return new Response({ message: "Form submitted successfully" });
+//     } catch (error) {
+//       console.error("Error sending email:", error);
+//       new Response({ message: "Failed to submit the form" });
+//     }
+//   } else {
+//     new Response({ message: "Method Not Allowed" });
+//   }
+// }
